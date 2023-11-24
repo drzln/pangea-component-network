ec5f6b41861c57e3493e403184951b42ca966c7e
luis <luisfelipemourapereira@protonmail.com>
2023-11-24 16:14:50 -0500

    entrypoint

--- Gemfile
+++ Gemfile
@@ -14,18 +14,7 @@ group :development do
 end
 
 group :production do
-  %w[
-    aws-sdk-dynamodb
-    terraform-synthesizer
-    abstract-synthesizer
-    tty-progressbar
-    tty-option
-    tty-table
-    tty-color
-    tty-box
-    toml-rb
-    bundler
-  ].each do |this_gem|
+  %w[terraform-synthesizer].each do |this_gem|
     gem this_gem
   end
 end
--- lib/pangea-component-network.rb
+++ lib/pangea-component-network.rb
@@ -1,4 +1,13 @@
-def render
+require %(terraform-synthesizer)
+
+################################################################################
+# external entities call render with data
+# render implements the synthesizer and processes
+# internal module with data
+################################################################################
+
+def render(_data)
+  synth = TerraformSynthesizer.new
   # return hash of rendered component
   {}
 end
