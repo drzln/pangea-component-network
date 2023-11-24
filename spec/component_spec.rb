f59310dc48d918690fb9ef231a9d2668a94c96c5
luis <luisfelipemourapereira@protonmail.com>
2023-11-24 16:57:49 -0500

    update gems

--- spec/component_spec.rb
+++ spec/component_spec.rb
@@ -2,16 +2,6 @@
 
 require_relative %(../lib/pangea-component-network)
 
-# RSpec.configure do |config|
-#   config.before(:suite) do
-#     nil
-#   end
-#
-#   config.after(:suite) do
-#     nil
-#   end
-# end
-
 describe 'component' do
   it 'should render' do
     expect(render({})).to be_kind_of(Hash)
